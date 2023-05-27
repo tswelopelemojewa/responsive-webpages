54d30718915e7ad2432e9abe12873a42c4912d97
Tswelopele Mojewa <tswelopelemojewa@gmail.com>
2023-05-27 14:56:59 +0200

    works

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,37 @@
-// import handlebars from 'vite-plugin-handlebars';
-// import data from "./data.json" assert {type: 'json'};
-
-// export default {
-//   plugins: [handlebars({
-//     context :{
-//       base: "/responsive_webpages/",
-//       data
-//     }
-//   })]
-// };
-
-
-import data from "./data.json" assert {type:'json'};
 import handlebars from 'vite-plugin-handlebars';
-import { defineConfig } from 'vite';
-// import data from './data.json';
+import data from "./data.json" assert {type: 'json'};
 
-export default defineConfig({
-    base : '/responsive_webpages/',
-    build: {
-        rollupOptions: {
-            input : {
-                main: 'index.html',
-                bootstrap: 'index-bootstrap.html',
-                materialize: 'index-materialize.html',
-            }
-        },
-    },
-    plugins: [handlebars({
-      context : {
-        data
-      }
+export default {
+      base: "/responsive_webpages/",
+  plugins: [handlebars({
+    context :{
+
+      data
+    }
   })]
+};
+
+
+// import data from "./data.json" assert {type:'json'};
+// import handlebars from 'vite-plugin-handlebars';
+// import { defineConfig } from 'vite';
+// // import data from './data.json';
+
+// export default defineConfig({
+//     base : '/responsive_webpages/',
+//     build: {
+//         rollupOptions: {
+//             input : {
+//                 main: 'index.html',
+//                 bootstrap: 'index-bootstrap.html',
+//                 materialize: 'index-materialize.html',
+//             }
+//         },
+//     },
+//     plugins: [handlebars({
+//       context : {
+//         data
+//       }
+//   })]
     
-});
+// });
